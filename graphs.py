464daa97317a8f35d499f6d88e066034fe2def7b
Alex Jasper <ajjasper@uncg.edu>
2024-04-25 00:30:14 -0400

    report added

--- graphs.py
+++ graphs.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np  # Import numpy for more control over mathematical functions
+
 
 # Create a DataFrame with the data
 data = {
